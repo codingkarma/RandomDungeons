bad7c9d8650092a73e25b613d57068992296ae58
codingkarma <jmorrisosu@gmail.com>
2014-11-18 21:04:48 -0500

    Changed bathing function definition which now returns a tuple of {vector,angle}.

--- Scripts/ai.js
+++ Scripts/ai.js
@@ -1,8 +1,9 @@
-function GetPathVector(startX, startZ, endX, endZ, tolerence)
+function GetDirectionAndAngle(startX, startZ, endX, endZ, tolerence)
 {
 	if(Math.abs(startX-endX) < tolerence || Math.abs(startZ-endZ) < tolerence)
 	{
-		new BABYLON.Vector3(0, 0, 0);
+		return {vector: new BABYLON.Vector3(0, 0, 0), angle: Math.acos((startX*endX + startZ*endZ)/(sqrt(startX^2 + endX^2) *sqrt(startZ^2 + endZ^2)));
 	}
-	return new BABYLON.Vector3(startX-endX, 0, startZ-endZ);
+	return {vector: new BABYLON.Vector3(startX-endX, 0, startZ-endZ), 
+		angle:Math.acos((startX * endX + startZ * endZ)/(sqrt(startX^2 + endX^2) *sqrt(startZ^2 + endZ^2)));
 }
